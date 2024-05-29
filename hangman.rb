558657d184b003d71d8931d5b44e1b670901cf8e
Fisola Adesuyi <adesuyifisola@gmail.com>
2024-05-29 13:47:15 +0200

    Player can start a new game, win or lost the game

--- hangman.rb
+++ hangman.rb
@@ -1,77 +1,152 @@
-# file  = File.new("google-10000-english-no-swears.txt")
-# file_array = file.readlines
-# code = file_array.sample.chomp!
-# p code
+require "json"
+module RandomWord
+  def self.random_word
+    file = File.new("google-10000-english-no-swears.txt")
+    read_file = file.readlines
+    sample = read_file.sample.chomp!
 
-# until code.length >= 5 && code.length <= 12
-#   # p file_array.sample
-#   code = file_array.sample.chomp!
-#   p code
-# end
-# p code
+    until sample.length >= 5 && sample.length <= 12
+      sample = read_file.sample.chomp!
+    end
+    sample
+  end
+end
+
+
+class Save
+  attr_reader :save_data
+  def initialize
+    @save_data = { save: [] }
+  end
+
+  def save_to_file(random_word, guess, attempt_left)
+    # @save_data = { save: [] }
+    # p File.exist?(file)
+    @save_data = { save: [] }
+    new_data = {
+      word: random_word,
+      attempt_left: attempt_left,
+      guess: guess
+    }
+
+    if File.exist?("save.json") && File.size("save.json") > 0
+      load_existing_file
+    else
+      @save_data = { save: [] }
+    end
+    @save_data[:save] << new_data
+    File.open("save.json", "w") { |file| file.write(@save_data.to_json) }
+  end
+
+  def load_existing_file
+    existing_data = JSON.parse(File.read("save.json"))
+    @save_data[:save] = existing_data["save"]
+  end
+end
 
 class Game
-  def initialize(file:, max_attempt: 10)
-    @file = file
+  attr_reader :max_attempt, :attempt, :new_game
+
+  RANDOMWORD = RandomWord.random_word
+  def initialize(max_attempt: 10, save:)
     @max_attempt = max_attempt
     @attempt = 0
+    @save = save
+    @save_instance = Save.new
+    puts "Start a new game? press 1 / Choose a saved game? press 2"
+    @choice = gets.chomp!
+    @new_game = @choice.to_s.include?("1")
+  end
 
-    read_file = @file.readlines
-    @code = read_file.sample.chomp!
+  def feedback(incorrect_letters, correct_letters)
+    puts "Inputed incorrect letters: #{incorrect_letters}"
+    puts "correct letters: #{correct_letters}"
+  end
 
-    until @code.length >= 5 && @code.length <= 12
-      @code = read_file.sample.chomp!
+  def attempt_left
+    attempt_left = max_attempt - @attempt
+    if attempt_left == 1
+      puts " 1 attempt left"
+    else
+      puts "#{attempt_left} attempts left"
+    end
+    attempt_left
+  end
+
+  # def save_game?(correct_letters, attempt_left)
+  #   # puts "Do you want to save the game and leave?"
+  #   # puts "y/n"
+  #   save_and_leave = gets.chomp.include?("y")
+  #   if save_and_leave
+  #     @save_instance.save_to_file(RANDOMWORD, correct_letters, attempt_left)
+  #     string_json = @save_instance.save_data.to_json
+  #     # File.write(@save, string_json, mode: 'w')
+  #     File.open("save.json", "w") { |file| file.write(string_json) }
+  #     # return "You can continue later. Bye!"
+  #   end
+  # end
+
+  def update_guess_word(last_index_letter, guess, correct_letters)
+    random_word = RANDOMWORD.chars
+    random_word.each_with_index do |letter, index|
+      if index == last_index_letter
+        substring = random_word[(last_index_letter + 1)..-1]
+        break if substring.index(guess).nil?
+        index_pos = last_index_letter + 1 + substring.index(guess)
+        correct_letters[index_pos] = guess
+        break
+      end
     end
   end
 
   def start
-    incorrect_letters = []
-    puts "Below is the number of letters to win the game in 10 attempts"
-    correct_letters = "-" * code.length
-    p "Correct letter will be displayed in the right position: #{correct_letters}"
-    puts "..............."
-    puts "..............."
-    puts
-
+    if new_game
+      # incorrect_letters
+      incorrect_cryptic_letter = []
+      puts "Below is the number of letters to win the game in 10 attempts"
+      cryptic_letter = "-" * RANDOMWORD.length
+      p "Correct letter will be displayed in the right position: #{cryptic_letter}"
+    end
 
     while @attempt < max_attempt
       puts
-      if max_attempt - @attempt == 1
-        puts " 1 attempt left"
-      else
-        puts "#{max_attempt - @attempt} attempts left"
-      end
-      puts "Inputed incorrect letters: #{incorrect_letters}"
-      puts "correct letters: #{correct_letters}"
+      attempt_left
+      feedback(incorrect_cryptic_letter, cryptic_letter)
       print "Guess a letter: "
       guess = gets.chomp!
-      if code.include?(guess)
-        puts "letter #{guess.downcase} is correct"
-        puts
-        index_pos = code.index(guess)
-        correct_letters[index_pos] = guess
 
-        if correct_letters.downcase == code.downcase
-          return "You won the game"
+      if RANDOMWORD.include?(guess)
+        puts
+        letter_position = RANDOMWORD.index(guess)
+        if cryptic_letter.include?(guess)
+          last_index_letter = cryptic_letter.rindex(guess)
+          update_guess_word(last_index_letter, guess, cryptic_letter)
+        else
+          cryptic_letter[letter_position] = guess
         end
-        puts "Inputed incorrect letters: #{incorrect_letters}"
-        puts "correct letters: #{correct_letters}"
+
         @attempt += 1
       else
         puts
-        puts "guess letter not correct"
-        incorrect_letters << guess
-        puts "Inputed incorrect letters: #{incorrect_letters}"
-        puts "correct letters: #{correct_letters}"
+        incorrect_cryptic_letter << guess
+        feedback(incorrect_cryptic_letter,  cryptic_letter)
         @attempt += 1
       end
-    end
-    puts "You lost, the code is #{code.downcase}"
-  end
 
-  attr_reader :max_attempt, :attempt, :code
+      if  cryptic_letter.downcase == RANDOMWORD.downcase
+        return "You win the game!"
+      end
+      puts "Do you want to save the game and leave?"
+      puts "y/n"
+      save_and_leave = gets.chomp.include?("y")
+      if save_and_leave
+        @save_instance.save_to_file(RANDOMWORD,  cryptic_letter, attempt_left)
+        return "Data saved, bye!"
+      end
+    end
+    puts "You lost, the RANDOMWORD is #{RANDOMWORD.downcase}"
+  end
 end
 
-file = File.new("google-10000-english-no-swears.txt")
-game = Game.new(file: file )
-game.start
+game = Game.new(save: "save.json")
+p game.start
