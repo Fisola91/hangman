1e4d9c1edfa220ec0e36fa82e1697ee54a43d36f
Fisola Adesuyi <adesuyifisola@gmail.com>
2024-05-30 18:34:42 +0200

    Player can start, win and lose the game

--- lib/random_word.rb
+++ lib/random_word.rb
@@ -1,12 +1,12 @@
 module RandomWord
   def self.random_word
     file = File.new("google-10000-english-no-swears.txt")
-    read_file = file.readlines
-    sample = read_file.sample.chomp!
+    file_data = file.readlines
+    word = file_data.sample.chomp!
 
-    until sample.length >= 5 && sample.length <= 12
-      sample = read_file.sample.chomp!
+    until word.length >= 5 && word.length <= 12
+      word = read_file.sample.chomp!
     end
-    sample
+    word
   end
 end
